3ed710ea229cfda290352ab815052de63ef4899e
Maria <ec39sp@yandex.ru>
2023-08-12 00:37:02 +0200

    hw33

---
+++ work2/hw3/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine());
+if (num < 6)
+    {
+       Console.WriteLine("Будний день");
+    }
+    else if (num<8)
+    {
+        Console.WriteLine("Выходной");
+    }
+    else
+    {
+         Console.WriteLine("Введите число от 1 до 7");
+    }
+
