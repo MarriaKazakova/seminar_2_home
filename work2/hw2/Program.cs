44d5d6dd984666bc8793fe083ee563ff01fd009b
Maria <ec39sp@yandex.ru>
2023-08-12 00:19:41 +0200

    hw2

--- work2/hw2/Program.cs
+++ work2/hw2/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿bool numberCheck(int num)
+{
+if (num < 100)
+return true;
+else return false;
+}
+
+int ThirdDidit (int num)
+{
+while (num>999)
+    {
+        num = num/10; 
+    }
+return num%10;
+}
+
+
+Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine());
+if (numberCheck(num))
+{
+    Console.WriteLine($"У данного числа нет третьей цифры, введите число больше или равное 100");
+}
+else
+{
+    int result = ThirdDidit(num);
+    Console.WriteLine($"Третьей цифрой числа {num} является {result}");
+}
+
