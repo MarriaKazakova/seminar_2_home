aa7f67908c499a35c5f749f975f74d6a8c5b1821
Maria <ec39sp@yandex.ru>
2023-08-11 23:03:34 +0200

    HW11

--- work2/hw1/Program.cs
+++ work2/hw1/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int SecondDigit(int num)
+{
+    int SecondDigit = (num % 100 - num % 10) / 10;
+    return SecondDigit;
+}
+
+int num = new Random().Next(100, 1000);
+int result = SecondDigit(num);
+Console.WriteLine($"Наше число {num} ");
+Console.WriteLine($"Вторая цифра числа {num} это {result}");
+
+
